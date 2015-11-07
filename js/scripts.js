8ab95e0e4a7468c6d6bcd857d17cb0eae2fe5f89
Jason Arnold <jason@thejasonfile.com>
2015-11-07 11:30:32 -0500

    fixed button classes

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 function playAnimation() {
-	$(this).remove();
 	$("#sun").animate({left: "1600px"}, 5000);
 	$("#word").css({"text-shadow": "rgb(0, 0, 0) -15px 15px 5px",
 					"transition-duration": "5s",
 					"transition-delay": "1.2s"});
 	setTimeout(function() {
-		//$("#repeat").addClass("visible").removeClass("hidden");
+		$("#repeat").addClass("visible").removeClass("hidden");
 	}, 5000);
 }
 
@@ -14,12 +13,17 @@ function resetAnimation() {
 	$("#word").css({"text-shadow": "rgb(0,0,0) 15px 15px 5px",
 					"transition-duration": "0.25s",
 					"transition-delay": "0.25s"});
+	setTimeout(function() {
+			$("#play").addClass("visible").removeClass("hidden");
+		}, 250);
 }
 
 $('#play').on("click", function() {
+	$(this).addClass('hidden').removeClass('visible');
 	playAnimation();
 });
 
 $('#repeat').on("click", function() {
+	$(this).addClass('hidden').removeClass('visible');
 	resetAnimation();
 });
