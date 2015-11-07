6a3c3c4c928b42b7bac5227e940541e6370ba718
Jason Arnold <jason@thejasonfile.com>
2015-11-07 11:17:30 -0500

    added play and reset transitions

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
-$('body').on("click", function() {
-	$("#sun").animate({left: "1600px"}, 5000)
-	$("#word").css("text-shadow", "rgb(0, 0, 0) -15px 15px 5px")
+function playAnimation() {
+	$(this).remove();
+	$("#sun").animate({left: "1600px"}, 5000);
+	$("#word").css({"text-shadow": "rgb(0, 0, 0) -15px 15px 5px",
+					"transition-duration": "5s",
+					"transition-delay": "1.2s"});
+	setTimeout(function() {
+		//$("#repeat").addClass("visible").removeClass("hidden");
+	}, 5000);
+}
+
+function resetAnimation() {
+	$("#sun").animate({left: "-100px"});
+	$("#word").css({"text-shadow": "rgb(0,0,0) 15px 15px 5px",
+					"transition-duration": "0.25s",
+					"transition-delay": "0.25s"});
+}
+
+$('#play').on("click", function() {
+	playAnimation();
 });
 
+$('#repeat').on("click", function() {
+	resetAnimation();
+});
